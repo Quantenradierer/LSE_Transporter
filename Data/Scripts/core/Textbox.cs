0e26274a3153a907cd50cfdea96cfbb256a0f717
Bernd Schmidt <leto.larp@googlemail.com>
2016-06-22 18:06:41 +0200

    adds textbox control

---
+++ Data/Scripts/core/Textbox.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sandbox.ModAPI;
+using VRage.ObjectBuilders;
+using Sandbox.ModAPI.Interfaces.Terminal;
+using VRage.ModAPI;
+
+namespace LSE.Control
+{
+    class Textbox<T> : BaseControl<T>
+    {
+        string DefaultValue;
+
+        public Textbox(
+            IMyTerminalBlock block,
+            string internalName,
+            string title,
+            string defaultValue)
+            : base(block, internalName, title)
+        {
+            DefaultValue = defaultValue;
+            CreateUI();
+        }
+
+        public override void OnCreateUI()
+        {
+            var control = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlTextbox, T>(InternalName);
+            control.Visible = ShowControl;
+            control.Getter = Getter;
+            control.Setter = Setter;
+            MyAPIGateway.TerminalControls.AddControl<T>(control);
+        }
+
+        public virtual StringBuilder Getter(IMyTerminalBlock block)
+        {
+            string value = "";
+            MyAPIGateway.Utilities.GetVariable<string>(block.EntityId.ToString() + InternalName, out value);
+
+            return new StringBuilder(value);
+        }
+
+        public virtual void Setter(IMyTerminalBlock block, StringBuilder builder)
+        {
+            MyAPIGateway.Utilities.SetVariable<string>(block.EntityId.ToString() + InternalName, builder.ToString());
+        }
+    }
+}
